ed08d181947a8a0898532c04b76f8a9d137643ab
etudiant <etudiant@I4-virtu>
2025-12-22 14:06:59 +0100

    creation fichiers gestion reservation et invitation MVC et debut programation

--- covoiturage/config/database.py
+++ covoiturage/config/database.py
@@ -18,5 +18,22 @@ def get_connection():
         conn = mysql.connector.connect(**DB_CONFIG)
         return conn
     except Error as e:
-        print("❌ Erreur de connexion MySQL :", e)
+        print(" Erreur de connexion MySQL :", e)
         return None
+    
+
+def get_trajet(self, id_trajet):
+     cursor = self.conn.cursor(dictionary=True)
+     cursor.execute(
+        "SELECT * FROM trajet WHERE id_trajet = %s",
+        (id_trajet,)
+     )
+     return cursor.fetchone()
+
+def insert_reservation(self, id_trajet, id_passager, statut):
+     cursor = self.conn.cursor()
+     cursor.execute(
+        "INSERT INTO reservation (id_trajet, id_passager, statut) VALUES (%s, %s, %s)",
+        (id_trajet, id_passager, statut)
+     )
+     self.conn.commit()
