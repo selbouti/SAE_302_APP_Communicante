897fcb15b968a13a2d96e9b1ac30e5f16f460eb8
etudiant <etudiant@I4-virtu>
2025-12-23 10:45:28 +0100

    commit avant modification arboressance

--- covoiturage/config/database.py
+++ covoiturage/config/database.py
@@ -5,7 +5,7 @@ DB_CONFIG = {
     "host": "localhost",
     "user": "covoiturage_app",
     "password": "MotDePasseSuperSecure123!",  # TODO: adapter
-    "database": "covoiturage",
+    "database": "covoiturage_2",
 }
 
 
@@ -22,18 +22,3 @@ def get_connection():
         return None
     
 
-def get_trajet(self, id_trajet):
-     cursor = self.conn.cursor(dictionary=True)
-     cursor.execute(
-        "SELECT * FROM trajet WHERE id_trajet = %s",
-        (id_trajet,)
-     )
-     return cursor.fetchone()
-
-def insert_reservation(self, id_trajet, id_passager, statut):
-     cursor = self.conn.cursor()
-     cursor.execute(
-        "INSERT INTO reservation (id_trajet, id_passager, statut) VALUES (%s, %s, %s)",
-        (id_trajet, id_passager, statut)
-     )
-     self.conn.commit()
--- covoiturage/main.py
+++ covoiturage/main.py
@@ -1,15 +1,27 @@
-# main.py
-import sys
-from PyQt5.QtWidgets import QApplication
-from views.main_window import MainWindow
+from flask import Flask
+from config.database import get_connection
+from controllers.gestion_res_controller import ReservationController
 
+def create_app():
+    """
+    Crée et configure l'application Flask.
+    """
+    app = Flask(__name__)
+
+    # Configuration de la base de données
+    db_connection = get_connection()
+
+    # Enregistrement des contrôleurs
+    ReservationController(app, db_connection)
+
+    return app
 
 def main():
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
-
+    """
+    Point d'entrée principal de l'application.
+    """
+    app = create_app()
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
     main()
